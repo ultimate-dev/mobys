eb124768e4c4af84b384507b715bde887d81a9e3
ultimate-dev <skrthbyk@gmail.com>
2022-10-24 07:06:07 +0300

    ...

--- mobys-api/src/app/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, Inject } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  async getHelloWorld() {
-    return 'Hello World!';
-  }
-}
--- mobys-api/src/database/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-import { Table, Column, Model } from 'sequelize-typescript';
-
-@Table({ modelName: 'user' })
-export class User extends Model {
-  @Column
-  name: string;
-}
